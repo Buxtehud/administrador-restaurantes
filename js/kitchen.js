8ce486ff1a70c3dd2d09f2da75e36e57b5e35406
Buxtehud <andres.anreyes@gmail.com>
2021-10-15 22:31:01 -0300

    Añadida funcionalidad al boton finish en cocina

--- js/kitchen.js
+++ js/kitchen.js
@@ -2,23 +2,35 @@ let kitchenOrder = JSON.parse(localStorage.getItem("kitchenOrder"));
 
 for(const mesa in kitchenOrder){
     let number = mesa.split("_")[1];
-    $("#kitchenContainer").append(`<div class="card">
-                                        <h3>Mesa ${number}</h3>
-                                        <p id="state${number}">${kitchenOrder[mesa].State}</p>
-                                        <table class="tableKitchen">
-                                            <tr>
-                                                <th>Producto</th>
-                                                <th>Cantidad</th>
-                                            </tr>
-                                            ${tableRows(mesa)}
-                                        </table>
-                                        <button id="start${number}">Iniciar</button>
-                                        <button id="finish${number}">Terminar</button>
-                                   </div>`);
-    $(`#start${number}`).click(()=>{
-        kitchenOrder[mesa].State = "Cocinando";
-        $(`#state${number}`).html(`${kitchenOrder[mesa].State}`);
-    });
+
+    if(kitchenOrder[mesa].State == "Enviado" || kitchenOrder[mesa].State == "Cocinando"){
+        $("#kitchenContainer").append(`<div class="card" id="card${number}">
+                                            <h3>Mesa ${number}</h3>
+                                            <p id="state${number}">${kitchenOrder[mesa].State}</p>
+                                            <table class="tableKitchen">
+                                                <tr>
+                                                    <th>Producto</th>
+                                                    <th>Cantidad</th>
+                                                </tr>
+                                                ${tableRows(mesa)}
+                                            </table>
+                                            <button id="start${number}">Iniciar</button>
+                                            <button id="finish${number}">Terminar</button>
+                                    </div>`);
+        $(`#start${number}`).click(() => {
+            kitchenOrder[mesa].State = "Cocinando";
+            $(`#state${number}`).html(`${kitchenOrder[mesa].State}`);
+            localStorage.setItem("kitchenOrder",JSON.stringify(kitchenOrder));
+        });
+
+        $(`#finish${number}`).click(() => {
+            kitchenOrder[mesa].State = "Terminado";
+            $(`#card${number}`).slideUp();
+            localStorage.setItem("kitchenOrder",JSON.stringify(kitchenOrder));
+            }
+        );
+
+    };
 };
 
 function tableRows(mesa){
